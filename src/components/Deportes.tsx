0d746b2c164323537a0c6f4f28fb9b687e28854f
JosePizarro1 <josepizarroarca@gmail.com>
2025-05-02 15:15:42 -0500

    uix2

--- src/components/Deportes.tsx
+++ src/components/Deportes.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import Link from "next/link";
+
 import { motion } from "framer-motion";
 import { FaRunning, FaGamepad, FaFutbol, FaSwimmer, FaChess,FaStar } from "react-icons/fa";
 
